322891238fef622517825f0253e6aa9214c21074
sakimotorin <sakimotorin@gmail.com>
2023-02-22 17:10:56 +0800

    deploy

---
+++ assets/NotFoundView-afeae716.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as c}from"./index-f654c997.js";const s={},n={class:"container"},r=c("div",{class:"my-3 text-center"}," Error 404 ",-1),a=[r];function _(d,i){return t(),o("div",n,a)}const l=e(s,[["render",_]]);export{l as default};
